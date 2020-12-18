2776645187bb2968fda244eedecebb7ea9869d44
willehpeh <will_alexander@me.com>
2020-12-18 15:34:06 +0100

    Server ready

---
+++ index.js
@@ -0,0 +1,55 @@
+module.exports = () => {
+
+  const uuid = require('uuid');
+  const faker = require('faker');
+  const avatarGen = require('random-avatar-generator');
+
+  const generator = new avatarGen.AvatarGenerator();
+
+  const data = {
+    users: [],
+    posts: [],
+    comments: []
+  };
+
+  for (let i = 0; i < 5; i++) {
+    data.users.push({
+      id: uuid.v4(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      image: generator.generateRandomAvatar(),
+      posts: [],
+      comments: []
+    });
+  }
+
+  for (let i = 0; i < 20; i++) {
+    const post = {
+      id: uuid.v4(),
+      userId: data.users[Math.floor(Math.random() * 5)].id,
+      content: faker.lorem.paragraph(),
+      comments: [],
+      created: faker.date.between('2021-01-01', '2021-01-31')
+    };
+    data.posts.push(post);
+    data.users.find(user => user.id === post.userId).posts.push(post.id);
+  }
+
+  for (let i = 0; i < 30; i++) {
+    const comment = {
+      id: uuid.v4(),
+      userId: data.users[Math.floor(Math.random() * 5)].id,
+      postId: data.posts[Math.floor(Math.random() * 20)].id,
+      content: faker.lorem.sentence(),
+      created: null
+    };
+    data.users.find(user => user.id === comment.userId).comments.push(comment.id);
+    const post = data.posts.find(post => post.id === comment.postId);
+    post.comments.push(comment.id);
+    comment.created = faker.date.between(post.created, '2021-01-31');
+    data.comments.push(comment);
+  }
+
+  return data;
+}
